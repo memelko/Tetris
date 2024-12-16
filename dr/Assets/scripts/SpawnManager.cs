c78af66ec04bd164060f98c8869032d8d28e713b
memelko <matus.ulohy@gmail.com>
2024-12-16 18:06:22 +0100

    done with spawn
    finished the spawning method and made them fit within the camera and reasonable size. Added empty spaces betwen them.

--- dr/Assets/scripts/SpawnManager.cs
+++ dr/Assets/scripts/SpawnManager.cs
@@ -8,7 +8,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject[,] pole = new GameObject[16,8];
     void Start()
     {
-        for(int i = 0; 1 < 16; i++) 
+        for(int i = 0; i < 16; i++) 
         {
           for ( int j=0; j < 8; j++)
           {
@@ -25,17 +25,17 @@ public class SpawnManager : MonoBehaviour
                 {
                     pole[i, j] = Instantiate(y, Vector3.zero, Quaternion.identity);
                 }
-                else if (typ > 3)
+                else 
                 {
                     pole[i, j] = Instantiate(e, Vector3.zero, Quaternion.identity);
                 }
           }
         }
-        for (int i = 0; 1 < 16; i++)
+        for (int i = 0; i < 16; i++)
         {
             for (int j = 0; j < 8; j++)
             {
-                pole[i, j].transform.position = new Vector3(3 - j * 0.65f, 4.5f - i * 0.65f, 0); 
+                pole[i, j].transform.position = new Vector3(3 - j * 0.65f, 4.5f - i * 0.5f, 0); 
                 
             }
         }
